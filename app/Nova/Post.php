27206afd9a72201299eb01a08948dc8f8a70dfef
tomascone <tomascone13@gmail.com>
2022-09-27 18:15:30 -0400

    Resource Authorization

--- app/Nova/Post.php
+++ app/Nova/Post.php
@@ -38,6 +38,11 @@ class Post extends Resource
         'id', 'title', 'body'
     ];
 
+    public static function indexQuery(NovaRequest $request, $query)
+    {
+        return $query->where('user_id', $request->user()->id);
+    }
+
     /**
      * Get the fields displayed by the resource.
      *
@@ -65,7 +70,9 @@ class Post extends Resource
                 'after_or_equal:publish_at'
             ]),
 
-            Boolean::make('Is Published'),
+            Boolean::make('Is Published')->canSee(function ($request) {
+                return false;
+            }),
 
             Select::make('Category')->options([
                 'tutorials' => 'Tutorials',
