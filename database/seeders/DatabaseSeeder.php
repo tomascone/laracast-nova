d244e1a248a4c949f4442f91be17ddfeaa8923d1
tomascone <tomascone13@gmail.com>
2022-09-27 16:28:14 -0400

    Defining Resources

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            'email' => 'tomascone13@gmail.com',
+            'password' => bcrypt('12345678'),
+        ]);
     }
 }
